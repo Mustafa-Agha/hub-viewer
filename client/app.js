d8d3208022593cf305e9419be2dc25b7fbb1ba8f
Mustafa-Agha <mustafaagha.developer@gmail.com>
2021-05-10 15:29:58 +0200

    finished app design

--- client/app.js
+++ client/app.js
@@ -1,19 +1,24 @@
-const { app, BrowserWindow } = require("electron");
+const { app, BrowserWindow, shell } = require("electron");
 const path = require("path");
 
 function createWindow() {
   const win = new BrowserWindow({
     width: 800,
     height: 300,
-    icon: path.join(__dirname, "./assets/images/favicon3.png"),
+    icon: path.join(__dirname, "./assets/images/favicon2.png"),
     webPreferences: {
       preload: path.join(__dirname, "preload.js"),
     },
   });
 
   win.loadFile("index.html");
+  win.removeMenu();
   win.center();
   win.setResizable(false);
+  win.webContents.on('new-window', function(e, url) {
+    e.preventDefault();
+    shell.openExternal(url);
+  });
 }
 
 app.whenReady().then(() => {
